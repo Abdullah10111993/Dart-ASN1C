8e7a8863fbfbac353a8135b979fefc370e507e89
Abdullah <abdullah.5121011@gmail.com>
2022-04-28 12:56:20 -0400

    Added library files

--- lib/src/dart_asn1c_base.dart
+++ lib/src/dart_asn1c_base.dart
@@ -1,6 +1,12 @@
 // TODO: Put public facing types in this file.
+import 'dart:ffi';
+import 'dart:io' show Directory, Platform;
+
+import 'package:ffi/ffi.dart';
+import 'package:path/path.dart' as path;
 
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class ASN1C {
+  typedef decodeNative = Pointer<Utf8> Function(Pointer<Utf8> str);
+  typedef decode = Pointer<Utf8> Function(Pointer<Utf8> str);
 }
