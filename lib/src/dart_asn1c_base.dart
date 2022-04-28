aef98c17ba7370af48ad048e82ede528b52afe22
Abdullah <abdullah.5121011@gmail.com>
2022-04-28 15:16:57 -0400

    Added asn1c decoder code

--- lib/src/dart_asn1c_base.dart
+++ lib/src/dart_asn1c_base.dart
@@ -5,8 +5,31 @@ import 'dart:io' show Directory, Platform;
 import 'package:ffi/ffi.dart';
 import 'package:path/path.dart' as path;
 
-/// Checks if you are awesome. Spoiler: you are.
+
+typedef DECODENATIVE = Pointer<Utf8> Function(Pointer<Utf8> str);
+typedef DECODE = Pointer<Utf8> Function(Pointer<Utf8> str);
+
 class ASN1C {
-  typedef decodeNative = Pointer<Utf8> Function(Pointer<Utf8> str);
-  typedef decode = Pointer<Utf8> Function(Pointer<Utf8> str);
+  
+  String libraryPath = "";
+
+  ASN1C(){
+    libraryPath = path.join(Directory.current.path, 'lib', 'src', 'asn1c_lib', 'lin_build', 'libasn1c.so');
+    if (Platform.isMacOS) {
+      libraryPath = path.join(Directory.current.path, 'lib', 'src', 'asn1c_lib', 'mac_build', 'libasn1c.dylib');
+    }
+    if (Platform.isWindows) {
+      libraryPath = path.join(Directory.current.path, 'lib', 'src', 'asn1c_lib', 'win_build', 'libasn1c.dll');
+    }
+  }
+
+  String decoder(String msg) {
+    final dylib = DynamicLibrary.open(libraryPath);
+    final decoder = dylib.lookupFunction<DECODENATIVE, DECODE>('decodeMsg');
+    final msgsUtf8 = msg.toNativeUtf8();
+    final decodedMsgUtf8 = decoder(msgsUtf8);
+    final decodedMsg = decodedMsgUtf8.toDartString();
+    return decodedMsg;
+  }
+
 }
--- test/dart_asn1c_test.dart
+++ test/dart_asn1c_test.dart
@@ -3,14 +3,15 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
+    final asn1c = ASN1C();
+    final msg = '00204B7FFFC2000004EA1292E54D8BD20439BF9E0D19E0FFFF00FF00000007D07D07F7FFF64039BF9E0D9B17A40833C0000780002000080000000007FFF807F801FFFC0012018000810102000000';
+  
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(asn1c.decoder(msg), isNotNull);
     });
   });
 }
